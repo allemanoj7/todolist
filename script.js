070c6f9065625f82f076ad59aca5d4e4be8a3544
allemanoj7 <136811007+allemanoj7@users.noreply.github.com>
2023-10-28 19:17:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,50 @@
-const form = document.getElementById('form')
-const input = document.getElementById('input')
-const todosUL = document.getElementById('todos')
-
-const todos = JSON.parse(localStorage.getItem('todos'))
-
-if(todos) {
-    todos.forEach(todo => addTodo(todo))
-}
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    addTodo()
-})
-
-function addTodo(todo) {
-    let todoText = input.value
-
-    if(todo) {
-        todoText = todo.text
+window.onload = function() {
+    loadTasksFromLocalStorage();
+};
+function addTask() {
+    const taskInput = document.getElementById("task");
+    const taskText = taskInput.value.trim();
+    if (taskText === "") {
+        alert("Please enter a task!");
+        return;
     }
 
-    if(todoText) {
-        const todoEl = document.createElement('li')
-        if(todo && todo.completed) {
-            todoEl.classList.add('completed')
-        }
+    const tasksList = document.getElementById("tasks");
+    const newTask = document.createElement("li");
+    newTask.innerHTML = `<label><input type="checkbox" onchange="updateTaskStatus(this)">${taskText}</label><button onclick='removeTask(this)'>Remove</button>`;
+    tasksList.appendChild(newTask);
 
-        todoEl.innerText = todoText
+    saveTasksToLocalStorage();
 
-        todoEl.addEventListener('click', () => {
-            todoEl.classList.toggle('completed')
-            updateLS()
-        }) 
-
-        todoEl.addEventListener('contextmenu', (e) => {
-            e.preventDefault()
-
-            todoEl.remove()
-            updateLS()
-        }) 
-
-        todosUL.appendChild(todoEl)
-
-        input.value = ''
-
-        updateLS()
-    }
+    taskInput.value = "";
 }
-
-function updateLS() {
-    todosEl = document.querySelectorAll('li')
-
-    const todos = []
-
-    todosEl.forEach(todoEl => {
-        todos.push({
-            text: todoEl.innerText,
-            completed: todoEl.classList.contains('completed')
-        })
-    })
-
-    localStorage.setItem('todos', JSON.stringify(todos))
+function removeTask(removeButton) {
+    const taskElement = removeButton.parentElement;
+    taskElement.remove();
+    saveTasksToLocalStorage();
+}
+function updateTaskStatus(checkbox) {
+    saveTasksToLocalStorage();
+}
+function saveTasksToLocalStorage() {
+    const tasksList = document.getElementById("tasks");
+    const tasks = [];
+    for (let i = 0; i < tasksList.children.length; i++) {
+        const taskText = tasksList.children[i].querySelector("label").textContent;
+        const isCompleted = tasksList.children[i].querySelector("input[type='checkbox']").checked;
+        tasks.push({ text: taskText, completed: isCompleted });
+    }
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+function loadTasksFromLocalStorage() {
+    const tasksList = document.getElementById("tasks");
+    const storedTasks = localStorage.getItem("tasks");
+    if (storedTasks) {
+        const tasks = JSON.parse(storedTasks);
+        tasks.forEach(task => {
+            const newTask = document.createElement("li");
+            newTask.innerHTML = `<label><input type="checkbox"${task.completed ? " checked" : ""} onchange="updateTaskStatus(this)">${task.text}</label><button onclick='removeTask(this)'>Remove</button>`;
+            tasksList.appendChild(newTask);
+        });
+    }
 }
